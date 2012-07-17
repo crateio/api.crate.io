3413e92a091d096504c1dae116e5ff3af2ae0776
Donald Stufft <donald.stufft@gmail.com>
2012-07-17 01:42:30 -0400

    Update configuration

--- warehouse.conf.py
+++ warehouse.conf.py
@@ -25,3 +25,16 @@ if "default" in config.emails:
 
 
 STATIC_URL = "https://dtl9zya2lik3.cloudfront.net/"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+
+AWS_ACCESS_KEY_ID = config.aws_access_key_id
+AWS_SECRET_ACCESS_KEY = config.aws_secret_access_key
+AWS_STORAGE_BUCKET_NAME = config.aws_storage_bucket_name
+
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_SECURE_URLS = False
+
+AWS_HEADERS = {
+    "Cache-Control": "max-age=31556926",
+}
