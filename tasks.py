e1babec1d8fa74d4bf853cfb5b09aaf881655ca9
Donald Stufft <donald.stufft@gmail.com>
2012-10-19 13:49:04 -0400

    Remove the hackish tasks for a good one

--- tasks.py
+++ tasks.py
@@ -1,53 +1,14 @@
-from invoke.task import task
-from invoke.run import run
-
-# REALLY HACKY - DON'T JUDGE ME
+from invoke import task, run
 
 
-def _deploy(target, force=False):
+@task
+def deploy(target, force=False):
+    if target is None:
+        raise Exception("Must provide a target")
+
     opts = []
 
     if force:
-        opts += ["f"]
+        opts += ["-f"]
 
-    run("git push {opts} {target} master".format(target=target, opts="".join(opts)))
-
-
-def _call_real(action, *args, **kwargs):
-    if args is None:
-        args = []
-
-    if kwargs is None:
-        kwargs = {}
-
-    g = globals()
-    name = "_{action}".format(action=action)
-
-    if name in g:
-        g[name](*args, **kwargs)
-
-
-_actions = set()
-
-
-@task
-def deploy():
-    _actions.add("deploy")
-
-
-@task
-def api():
-    if not _actions:
-        raise Exception("Must call an action prior to selection a target")
-
-    for action in _actions:
-        _call_real(action, "api")
-
-
-@task
-def app():
-    if not _actions:
-        raise Exception("Must call an action prior to selection a target")
-
-    for action in _actions:
-        _call_real(action, "app")
+    run("git push {opts} {target} master".format(target=target, opts=" ".join(opts)))
