f76023be2bed67acb41a61ed2db14930d92af364
Donald Stufft <donald.stufft@gmail.com>
2012-10-20 23:57:58 -0400

    Add the db.dump command to invoke

--- tasks.py
+++ tasks.py
@@ -1,4 +1,19 @@
+import contextlib
+import os
+import shutil
+import tempfile
+
 from invoke import task, run
+
+
+@contextlib.contextmanager
+def TemporaryDirectory(*args, **kwargs):
+    directory = tempfile.mkdtemp(*args, **kwargs)
+
+    try:
+        yield directory
+    finally:
+        shutil.rmtree(directory)
 
 
 @task
@@ -12,3 +27,21 @@ def deploy(target, force=False):
         opts += ["-f"]
 
     run("git push {opts} {target} master".format(target=target, opts=" ".join(opts)))
+
+
+@task(aliases=["db.dump"])
+def db_dump(db="warehouse", create=True, drop=False):
+    url = run("heroku pgbackups:url --app crate-warehouse", hide="out").stdout
+
+    with TemporaryDirectory() as tmp:
+        location = os.path.join(tmp, "crate.dump")
+
+        run("curl -o {location} '{url}'".format(url=url, location=location))
+
+        if drop:
+            run("dropdb {db}".format(db=db))
+
+        if create:
+            run("createdb {db}".format(db=db))
+
+        run("pg_restore --verbose --clean --no-acl --no-owner -d {db} {location}".format(db=db, location=location))
